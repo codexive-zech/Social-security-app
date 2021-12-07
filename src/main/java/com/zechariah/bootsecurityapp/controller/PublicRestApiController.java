11c752bcbf2380432758f07869e3b77776dfb75c
Codexive_zech <zechariahhounwanou@gmail.com>
2021-12-07 13:20:37 +0100

    added Database configuration

--- src/main/java/com/zechariah/bootsecurityapp/controller/PublicRestApiController.java
+++ src/main/java/com/zechariah/bootsecurityapp/controller/PublicRestApiController.java
@@ -1,13 +1,21 @@
 package com.zechariah.bootsecurityapp.controller;
 
 
+import com.zechariah.bootsecurityapp.db.UserRepository;
+import com.zechariah.bootsecurityapp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/public")
 public class PublicRestApiController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("test1")
     public String test1(){
@@ -17,5 +25,10 @@ public class PublicRestApiController {
     @GetMapping("test2")
     public String test2(){
         return "API Test 2, Admin";
+    }
+
+    @GetMapping("users")
+    public List<User> getAll(){
+        return userRepository.findAll();
     }
 }
